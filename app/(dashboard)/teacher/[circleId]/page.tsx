7331472a1e03ec43af1e59563a24a65411a9315d
Mohamad Bashir <mbashirkuja@gmail.com>
2026-09-03 23:47:38 +0300

    Update page.tsx

--- app/(dashboard)/teacher/[circleId]/page.tsx
+++ app/(dashboard)/teacher/[circleId]/page.tsx
@@ -3,6 +3,6 @@ import React from 'react'
 export default async function page({params}: {params: {circleId : string}}) {
     const {circleId} = await params;
   return (
-    <div>page: {circleId}</div>
+    <div>circle id : {circleId}</div>
   )
 }
